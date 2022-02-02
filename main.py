461aedf8d8818400efa5ed5d4e2aa26304b89f94
Storm_gid <gideaosa15@gmail.com>
2022-02-02 19:19:39 -0300

    yha yha

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from vosk import Model, KaldiRecognizer
+import os
+import pyaudio
+import pyttsx3
+import json
+import core
+
+# Síntise de  fala
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+#reconheciimento de fala
+model = Model('model')
+rec = KaldiRecognizer(model, 16000)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
+stream.start_stream()
+
+#reconhecimento de fala infinito
+while True:
+    data = stream.read(2048)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        result = rec.Result()
+        result = json.loads(result)
+
+        if result is not None:
+            text = result['text']
+
+            print(text)
+
+            if text == 'que horas são' or text == 'me diga a hora':
+                speak(core.SystemInfo.get_time())
+
